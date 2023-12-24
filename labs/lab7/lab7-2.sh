4c6c6afe21cd9dd26a4ab23695a23f90d04be294
Roman_Ubuntu <roman_boyko.48@mail.ru>
2023-12-25 01:09:11 +0300

    lab7

--- labs/lab7/lab7-2.sh
+++ labs/lab7/lab7-2.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+grep -rn -E --include=\*.c --include=\*.h '^\S+$' "$1" | wc -l
--- labs/lab7/lab7-4.py
+++ labs/lab7/lab7-4.py
@@ -13,6 +13,7 @@ def memo(old_func):
     return new_func
 
 
+@memo
 def fib(n):
     if n < 2:
         return 1
